fea450f3a16c498e971354ecfc25feffcb8d730c
Mystic Gems <mystic.gems.sl@gmail.com>
2014-11-13 20:22:06 -0500

    Images without rename, mcjTeleBlender import

--- misc_toolbar.py
+++ misc_toolbar.py
@@ -18,7 +18,7 @@
 
 bl_info = {
     "name": "Jack's Tools",
-    "version": (0, 1),
+    "version": (0, 2),
     "author": "Jack of Spades",
     "blender": (2, 72, 0),
     "description": "Miscellaneous tools",
@@ -47,7 +47,7 @@ def findImage(name):
 # - not used in any material node
 class cleanupExtraImages(bpy.types.Operator):
     """Clean up extra images in imports"""
-    bl_label = "Clean up images"
+    bl_label = "With Rename"
     bl_idname = "view3d.clean_up_images"
 
     # Do that thing
@@ -92,6 +92,70 @@ class cleanupExtraImages(bpy.types.Operator):
         self.report({'INFO'}, "Removed: %s images" % count)
         return{'FINISHED'}
 
+# Removes duplicate images, defined as:
+# - not used in any material node
+class cleanupExtraImagesNoRename(bpy.types.Operator):
+    """Clean up extra images in imports"""
+    bl_label = "No Rename"
+    bl_idname = "view3d.clean_up_images_no_rename"
+
+    # Do that thing
+    def execute(self, context):
+        print('new run')
+        img_names = []
+        materials = bpy.data.materials
+
+        # find images in material nodes
+        for mat in materials:
+            if hasattr( mat, 'node_tree' ):
+                if hasattr( mat.node_tree, 'nodes' ):
+                    nodes = mat.node_tree.nodes
+                    print( mat.name )
+                    for node in nodes:
+                        if type(node) == bpy.types.ShaderNodeTexImage:
+                            # Get the image name
+                            try:
+                                nam = node.image.name
+                                print( "- Using image " + nam)
+                                img_names.append(nam)
+                            except:
+                                print('- No image')
+         
+        # Iterate through all images and remove those we
+        # haven't identified as used
+        imgs = bpy.data.images
+        count = 0
+        for image in imgs:
+            name = image.name
+            if name not in img_names:
+                print( 'Removing image ' + name )
+                count = count + 1
+                image.user_clear()
+        self.report({'INFO'}, "Removed: %s images" % count)
+        return{'FINISHED'}
+    
+class loadTeleBlenderExport(bpy.types.Operator):
+    """Import mcjTeleBlender export from my usual location"""
+    bl_label = "mcjTeleBlender"
+    bl_idname = "view3d.jack_import"
+    
+    def execute(self, context):
+        filename = '/Users/jspade/Documents/DAZ 3D/Studio/Exports/scene.py'
+        print('Importing from ' + filename)
+        exec(compile(open(filename).read(), filename, 'exec'))
+#        text = bpy.data.texts.load(path)
+#
+#        for area in bpy.context.screen.areas:
+#           if area.type == 'TEXT_EDITOR':
+#                area.spaces[0].text = text # make loaded text file visible
+#
+#                ctx = bpy.context.copy()
+#                ctx['edit_text'] = text
+#                ctx['area'] = area
+#                ctx['region'] = area.regions[-1]
+#                bpy.ops.text.run_script(ctx)
+        return{'FINISHED'}
+
 # Create the toolbar panel
 class JackPanel(bpy.types.Panel):
     """Miscellaneous personal tools"""
@@ -103,13 +167,21 @@ class JackPanel(bpy.types.Panel):
          layout = self.layout
          
          row = layout.row()
+         row.label(text="Import")
+         
+         split = layout.split()
+         col = split.column( align = True )
+         col.operator("view3d.jack_import")
+         
+         row = layout.row()
          row.label(text="Global Cleanup")
 
          split = layout.split()
          col = split.column( align = True )
          # Button for image cleanup
          col.operator("view3d.clean_up_images", icon='IMAGE_DATA')
-         
+         col.operator("view3d.clean_up_images_no_rename", icon='IMAGE_DATA')
+                
 #============================================================================
 # REGISTER AND UNREGISTER
 #============================================================================
@@ -117,9 +189,13 @@ class JackPanel(bpy.types.Panel):
 def register():
     bpy.utils.register_class(JackPanel)
     bpy.utils.register_class(cleanupExtraImages)
+    bpy.utils.register_class(cleanupExtraImagesNoRename)
+    bpy.utils.register_class(loadTeleBlenderExport)
 def unregister():
     bpy.utils.unregister_class(JackPanel)
     bpy.utils.unregister_class(cleanupExtraImages)
+    bpy.utils.unregister_class(cleanupExtraImagesNoRename)
+    bpy.utils.unregister_class(loadTeleBlenderExport)
 
 if __name__ == "__main__":
     register()
